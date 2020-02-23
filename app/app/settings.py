e597b4409b723cd0af898d97807639868deba895
Andres Vargas <andrevargas@coca-cola.com>
2020-02-23 16:55:59 -0600

    put permission

--- app/app/settings.py
+++ app/app/settings.py
@@ -124,11 +124,12 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-        'DEFAULT_AUTHENTICATION_CLASSES': [
-                    'rest_framework.authentication.TokenAuthentication',
-                ],
-        'DEFAULT_PERMISSION_CLASSES':(
-                            'rest_framework.permissions.IsAuthenticated',
-                ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
 }
 
--- app/core/views.py
+++ app/core/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .serializers import AccountSerializer, HostSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Account, Host
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class AccountViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
 
@@ -20,4 +24,11 @@ class HostViewset(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(account_id = self.kwargs["accounts_pk"])
+
+
+    def retrieve(self, request, pk=None, accounts_pk=None):
+        qs = Host.objects.all().filter(account_id= accounts_pk)
+        host = get_object_or_404(qs, pk=pk)
+
+        return Response("")
 
