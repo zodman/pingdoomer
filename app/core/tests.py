5e01e8c2f54285210e423bcf8976bb7daed90954
Andres Vargas <andrevargas@coca-cola.com>
2020-03-08 23:45:42 -0600

    test update

--- app/core/tests.py
+++ app/core/tests.py
@@ -11,18 +11,16 @@ class TestRest(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         self.faker = faker.Faker()
 
-
     def _create_account(self):
-        data = dict(name="andres", external_id=1)
+        data = dict(name=self.faker.name(),
+                    external_id=self.faker.random_digit())
         self.post("account-list",data=data)
         self.response_201()
         return self.last_response.json()
 
-    def test_create(self):
+    def _create_hosts(self):
         self.get_check_200("account-list")
-
         resp = self._create_account()
-
         id =  resp.get("id", False)
         self.assertTrue(id, resp)
 
@@ -33,12 +31,22 @@ class TestRest(APITestCase):
             'hostname':self.faker.domain_name(),
         }
         self.post("account-hosts-list", accounts_pk=id, data=data)
-        host_id = self.last_response.json().get("id")
+        return self.last_response.json()
+
+    def test_create(self):
+        self._create_account()
+    def test_create_host(self):
+        resp = self._create_account()
+        id =  resp.get("id", False)
+        resp = self._create_hosts()
+
+    def test_host_detail(self):
+        resp = self._create_account()
+        id =  resp.get("id", False)
+        resp = self._create_hosts()
+        host_id = resp.get("id")
         self.get("account-hosts-detail", accounts_pk=id, pk = host_id)
         self.response_200()
         resp = self.last_response.json()
         self.assertTrue(resp)
-
-
-
 
